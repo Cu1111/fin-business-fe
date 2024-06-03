fcbd2d05eb6537360af60662ef06475ab1ba4976
cuijiaxing <321923502@qq.com>
2024-06-03 23:49:11 +0800

    feat: dataFetch 和 select封装完成

--- src/components/formSelect/index.tsx
+++ src/components/formSelect/index.tsx
@@ -1,1 +1,50 @@
-import { Select } from '@arco-design/web-react';
+import React, { useState, useRef } from 'react';
+import { Select, Spin } from '@arco-design/web-react';
+import debounce from 'lodash/debounce';
+
+const FormSelect = (props) => {
+  const { value, onChange, onFetchData, ...others } = props;
+  const [options, setOptions] = useState([]);
+  const [fetching, setFetching] = useState(false);
+
+  const refFetchId = useRef<number>();
+
+  const debouncedFetchData = debounce(async (filter) => {
+    refFetchId.current = Date.now();
+    const fetchId = refFetchId.current;
+    setFetching(true);
+    setOptions([]);
+    onFetchData(filter).then((data) => {
+      if (refFetchId.current === fetchId) {
+        setFetching(false);
+        setOptions(data);
+      }
+    });
+  }, 200);
+
+  return (
+    <Select
+      {...others}
+      value={value}
+      onChange={onChange}
+      options={options}
+      notFoundContent={
+        fetching ? (
+          <div
+            style={{
+              display: 'flex',
+              alignItems: 'center',
+              justifyContent: 'center',
+            }}
+          >
+            <Spin style={{ margin: 12 }} />
+          </div>
+        ) : null
+      }
+      onSearch={debouncedFetchData}
+    />
+  );
+};
+
+FormSelect.displayName = 'FormSelect';
+export default FormSelect;
--- src/utils/DataFetch.ts
+++ src/utils/DataFetch.ts
@@ -1,9 +1,14 @@
-const DataFetch = (
-  url: string,
-  params?: any,
-  callback?: any,
-  options?: any
-) => {
+import $fetch from './$fetch';
 
-  
+// TODO: 后续可以增加更多配置使filter可调整
+const DataFetch = (url: string, params?, callback?) => {
+  return async function (filter) {
+    const data = await $fetch(url, { filter, ...params });
+    if (callback) {
+      return callback(data);
+    }
+    return data;
+  };
 };
+
+export default DataFetch;
